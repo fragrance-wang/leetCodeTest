07f664649e0fd4ad6c9482c082b8ad70fb62fc13
wangshan <1872672106@qq.com>
2023-07-25 16:44:49 +0800

    没啥

---
+++ src/main/java/com/atws/simple/minCostClimbingStairsTest.java
@@ -0,0 +1,18 @@
+package com.atws.simple;
+
+/**
+ *
+ * https://leetcode.cn/problems/min-cost-climbing-stairs/
+ * @author wangshan
+ * @date 2023-07-25 下午3:56
+ */
+public class minCostClimbingStairsTest {
+    public static void main(String[] args) {
+
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+
+        return 0;
+    }
+}
